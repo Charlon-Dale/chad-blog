2bbcfbac160abc627191c447a53b6a620bf07db5
Charlon-Dale <charlondalesanagustin@gmail.com>
2022-11-21 11:27:38 +0800

    added ref

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,20 @@
 <template>
   <div class="home">
     home
-    <p>My name is {{ name }} and my age is {{ age }}</p>
+    <p ref="p">My name is {{ name }} and my age is {{ age }}</p>
     <button @click="handleClick">Click me</button>
   </div>
 </template>
 
 <script>
+import { ref } from 'vue'
+
 export default {
   name: 'Home',
   setup() {
-    console.log('setup')
+    console.log(this)
+
+    const param = ref(null)
 
     let name = 'mario'
     let age = 30
@@ -19,7 +23,7 @@ export default {
       console.log('You clicked me')
     }
 
-    return { name, age, handleClick }
+    return { name, age, handleClick, p }
   },
   data() {
     return {
