2349f70bef6d53ad413b72cccb91959c15c1f832
Charlon-Dale <charlondalesanagustin@gmail.com>
2022-11-21 11:10:47 +0800

    added button click event

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,7 @@
   <div class="home">
     home
     <p>My name is {{ name }} and my age is {{ age }}</p>
+    <button @click="handleClick">Click me</button>
   </div>
 </template>
 
@@ -14,8 +15,16 @@ export default {
     let name = 'mario'
     let age = 30
 
-    return { name: name, age: age }
+    const handleClick = () => {
+      console.log('You clicked me')
+    }
 
+    return { name, age, handleClick }
+  },
+  data() {
+    return {
+      age: 40
+    }
   }
 }
 </script>
