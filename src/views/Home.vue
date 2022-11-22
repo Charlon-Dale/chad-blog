79350c311cce4328ef3828cac950249f173e6f03
Charlon-Dale <charlondalesanagustin@gmail.com>
2022-11-22 11:31:03 +0800

    used computed value

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,35 +1,22 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <h2>Refs</h2>
-    <p>{{ ninjaOne.name }} - {{ ninjaOne.age }}</p>
-    <button @click="updateNinjaOne">Update Ninja One</button>
-    <h2>Reactive</h2>
-    <p>{{ ninjaTwo.name }} - {{ ninjaTwo.age }} - {{ nameTwo }}</p>
-    <button @click="updateNinjaTwo">Update ninja two</button>
+    <p> {{ name }} </p>
+    
   </div>
 </template>
 
 <script>
-import { ref, reactive } from 'vue'
+import { ref, computed } from 'vue'
 
 export default {
   name: 'Home',
   setup() {
-    const ninjaOne = ref({ name: 'mario', age: 30 })
-    const ninjaTwo = reactive({ name: 'luigi', age: 35 })
+    const name = computed(() => {
+      return 'shaun'
+    })
 
-    const nameOne = ref('mario')
-    const nameTwo = reactive('luigi')
-
-    const updateNinjaOne = () => {
-      ninjaOne.value.age = 40
-    }
-    
-    const updateNinjaTwo = () => {
-      ninjaTwo.age = 45
-    }
-    return { ninjaOne, updateNinjaOne, ninjaTwo, updateNinjaTwo, nameOne, nameTwo }
+    return { name }
   }
 }
 </script>
