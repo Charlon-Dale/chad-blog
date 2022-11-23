46d5530d287bc951c4e0615036bd7072281552dc
Charlon-Dale <charlondalesanagustin@gmail.com>
2022-11-23 13:39:00 +0800

    created setup and template for PostList

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,11 +1,17 @@
 <template>
     <div class="post-list">
-        
+        <div v-for="post in posts" :key="post.id">
+            <h3>{{ post.title }}</h3>
+        </div>
     </div>
 </template>
 
 <script>
 export default {
+    props: ['posts'],
+    setup(props) {
+        console.log(props.posts)
+    }
 
 }
 </script>
