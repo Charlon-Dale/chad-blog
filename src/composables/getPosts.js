89398933a7639a3c00ac09457a8ea6aacf11360c
Charlon-Dale <charlondalesanagustin@gmail.com>
2022-11-25 16:46:59 +0800

    fixed the getPost issue

--- src/composables/getPosts.js
+++ src/composables/getPosts.js
@@ -1,15 +1,15 @@
 import { ref } from 'vue'
 
 const getPosts = () => {
-    const posts = ref([])
-    const error = ref(null)
+  const posts = ref([])
+  const error = ref(null)
 
-    const load = async () => {
-      try {
-        let data = await fetch('http://localhost:3000/posts')
-        if (!data.ok) {
-          throw Error('no data available')
-        }
+  const load = async () => {
+    try {
+      let data = await fetch('http://localhost:3000/posts')
+      if (!data.ok) {
+        throw Error('no data available')
+      }
         posts.value = await data.json()
       }
       catch (err) {
@@ -18,7 +18,7 @@ const getPosts = () => {
       }
     }
 
-  return { posts , error, load }
+  return { posts, error, load }
 }
 
 export default getPosts
